8565384c923956112b9db3d6ea16167c0b5add3e
Nick Hershman <nick_hershman@beaverton.k12.or.us>
2023-03-28 13:34:49 -0400

    Better create .zip for project.

--- get-zip.sh
+++ get-zip.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 # -x excludes 
-zip -r "../bsd-copy-paste-${1}.zip" * -x get-zip.sh .git/* dev-store/*
+zip -r "../bsd-copy-paste-${1}.zip" * -x get-zip.sh .git/* dev-store/* dev-store/ .git/
