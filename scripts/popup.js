7f7ddbc4408f32b050977e51faacd66c14039a9a
Nick Hershman <nick_hershman@beaverton.k12.or.us>
2023-11-10 20:24:37 -0800

    Adds DataTables and makeSubmissionTable() call to create a table of paste-able values. Needs formatting help. Want headers to be rotated and shrink width or allow horizontal scroll?

--- scripts/popup.js
+++ scripts/popup.js
@@ -639,10 +639,64 @@ function getCourseIdFromAssignments(assignments) {
     return assignments[0].course_id
 }
 
+
+function makeSubmissionsTable(submissions, rubrics) {
+    // display all submissions accounting for different types of data available.
+    // submissions may have scores for rubrics but maybe not...
+    //table headers: sortable_name, r1, r2,..., r_n, entered_score, missing, late, excused
+    console.log(`Making submissions table with ${submissions.length} submissions and ${rubrics.length} rubrics.`)
+    my_table = `<table id="submissions">\n`
+    my_table += '<thead><tr><td>Name</td>'
+    rubrics.forEach((r) => {
+        my_table += `<td class="rotate"><div>${r.alt_code}</div></td>`
+    })
+    let my_cols = ['points','missing','late','excused']
+    my_cols.forEach((col) => {
+        my_table += `<td class="rotate"><div>${col}</div></td>`
+    })
+    my_table += `</tr></thead>\n<tbody>`
+    submissions.forEach((s) => {
+        my_table += `<tr><td>${s.sortable_name}</td>`
+        if ('rubric_assessment' in s) {
+            
+            rubrics.forEach((r) =>{
+                let rubric_score = ''
+                try {
+                    rubric_score = s.rubric_assessment[r.id].points
+                } catch(e) {
+                    console.log(`Error parsing rubric ${e}`)
+                }
+                my_table += `<td>${rubric_score}</td>`
+        })
+        } else {
+            rubrics.forEach((r) => {
+                // no rubric assessment in submission
+                my_table += '<td></td>'
+            })
+        }
+
+        my_table += `<td>${s.entered_score}</td><td>${s.missing}</td><td>${s.late}</td><td>${s.excused}</td></tr>`
+    })
+
+    my_table += '</tbody></table>'
+
+    $('div#assign_submissions').append(my_table)
+    var table = new DataTable('table#submissions', {
+        order: [[1, 'asc']]
+    })
+
+    $('table#submissions').addClass('display compact')
+
+    // table.columns.adjust().draw()
+
+    
+}
+
 function update_rubric_list(assignments, submissions) {
     let assign_id = getAssignmentIdFromSubmissions(submissions)
     let assignment = getAssignmentById(assignments, assign_id)
     let rubrics = getRubrics(assignment)
+    console.log('Rubrics:',rubrics)
 
     // show rubrics for assignment
     $('div#rubrics_overview').html("<h4>Submissions/Scores for ALT's</h4><ul></ul>")
@@ -661,29 +715,26 @@ function update_rubric_list(assignments, submissions) {
         let summary = countScoresFromSubmissionsByRubricId(submissions, r.id)
         console.log('Summary from countScores...',summary)
 
-        let alert_student_total = 0
-        let my_list = []
-        let my_keys = ['no_scores', 'skipped_entirely']
-        my_keys.forEach((key) => {
-            alert_student_total += summary[key].length
+        $('div#rubrics_overview').append('<li></li>').html(`<b>${r.alt_code}</b> - ${r.alt_text} (<em>${summary.rubric_scores.length} scores and ${summary.missing.length} missing</em>)`)
 
-            summary[key].forEach((s) => {
-                my_list.push(`${s.sortable_name} (${s.synergy_id})`)
-            })
-        })
+        // let alert_student_total = 0
+        // let my_list = []
+        // let my_keys = ['no_scores', 'skipped_entirely']
+        // my_keys.forEach((key) => {
+        //     alert_student_total += summary[key].length
 
-        let my_text = `<ul><li><b>${alert_student_total} missing Rubric Scores</b>:<br />${my_list.sort().join("<br />")}</li>`
-        
-        let li = $('<li></li>').html(`<b>${r.alt_code}</b> - ${r.alt_text} (<em>${summary.rubric_scores.length} scores and ${summary.missing.length} missing</em>)`)
-        
-        li.append($('<ul></ul>').append(my_text))
-        // Object.keys(summary).forEach((key) => {
-        
-        
-        
+        //     summary[key].forEach((s) => {
+        //         my_list.push(`${s.sortable_name} (${s.synergy_id})`)
+        //     })
+        // })
+        // let my_text = `<ul><li><b>${alert_student_total} missing Rubric Scores</b>:<br />${my_list.sort().join("<br />")}</li>`
+        // let li = $('<li></li>').html(`<b>${r.alt_code}</b> - ${r.alt_text} (<em>${summary.rubric_scores.length} scores and ${summary.missing.length} missing</em>)`)
+        // li.append($('<ul></ul>').addClass('student_list').append(my_text))
+        // $('div#rubrics_overview ul').append(li)
 
-        $('div#rubrics_overview ul').append(li)
         // TODO create display for students with scores and missing scores...
+
+        // See https://datatables.net/examples/basic_init/table_sorting.html
 
         
     })   
@@ -697,6 +748,11 @@ async function process_assign_change(course_id, assignments) {
     let assignment = getAssignmentById(assignments, assign_id)
     let submissions = await getSubmissions(course_id, assignments, assignment.id)
     update_rubric_list(assignments, submissions)
+    makeSubmissionsTable(submissions, getRubrics(assignment))
+    // $('div#assign_submissions').append(makeSubmissionsTable(submissions, getRubrics(assignment)))
+    // new DataTable('table#submissions', {
+    //     order: [[1, 'asc']]
+    // })
 }
 
 
@@ -847,8 +903,10 @@ chrome.runtime.sendMessage(my_message, (response) => {
             update_rubric_list(assignments, submissions)
             let assign_id = getAssignmentIdFromSubmissions(submissions)
             update_assign_select(assignments, assign_id)
+            let assignment = getAssignmentById(assignments, assign_id)
             update_submissions_overview(submissions)
             updateRounding(roundUpFrom)
+            makeSubmissionsTable(submissions, getRubrics(assignment))
         } else {
             console.log(`submissions received but no assignments sent... problem!`)
         }
